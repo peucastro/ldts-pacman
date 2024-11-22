06acdb2433257c81e5ac126c7d03b095b66ef86d
Paulo Saavedra <paulo.saa2005@gmail.com>
2024-11-22 23:23:55 +0000

    new Arena tests

--- src/test/java/pt/up/fe/ldts/pacman/model/game/ArenaTest.java
+++ src/test/java/pt/up/fe/ldts/pacman/model/game/ArenaTest.java
@@ -1,8 +1,9 @@
 package pt.up.fe.ldts.pacman.model.game;
 
 import org.junit.jupiter.api.Test;
+import pt.up.fe.ldts.pacman.model.game.element.ghost.*;
+import pt.up.fe.ldts.pacman.model.game.element.item.*;
 import pt.up.fe.ldts.pacman.model.game.element.pacman.Pacman;
-import pt.up.fe.ldts.pacman.model.game.element.item.Coin;
 import pt.up.fe.ldts.pacman.model.game.element.Wall;
 
 import java.util.HashSet;
@@ -19,15 +20,10 @@ public class ArenaTest {
         Arena arena = new Arena(width, height);
 
         assertNotNull(arena.getPacman());
-        assertNotNull(arena.getBlinky());
-        assertNotNull(arena.getPinky());
-        assertNotNull(arena.getInky());
-        assertNotNull(arena.getClyde());
         assertEquals(width, arena.getWidth());
         assertEquals(height, arena.getHeight());
         assertTrue(arena.getWalls().isEmpty());
-        assertTrue(arena.getCoins().isEmpty());
-        assertTrue(arena.getPowerUps().isEmpty());
+        assertTrue(arena.getCollectibles().isEmpty());
     }
 
     @Test
@@ -42,34 +38,6 @@ public class ArenaTest {
         assertEquals(2, arena.getWalls().size());
         assertTrue(arena.getWalls().contains(new Wall(new Position(1, 1))));
         assertTrue(arena.getWalls().contains(new Wall(new Position(2, 2))));
-    }
-
-    @Test
-    void testSetAndGetCoins() {
-        Arena arena = new Arena(20, 15);
-        HashSet<Coin> coins = new HashSet<>();
-        coins.add(new Coin(new Position(1, 1)));
-        coins.add(new Coin(new Position(3, 4)));
-
-        arena.setCoins(coins);
-
-        assertEquals(2, arena.getCoins().size());
-        assertTrue(arena.getCoins().contains(new Coin(new Position(1, 1))));
-        assertTrue(arena.getCoins().contains(new Coin(new Position(3, 4))));
-    }
-
-    @Test
-    void testSetAndGetPowerUps() {
-        Arena arena = new Arena(20, 15);
-        HashSet<PowerUp> powerUps = new HashSet<>();
-        powerUps.add(new PowerUp(new Position(5, 5)));
-        powerUps.add(new PowerUp(new Position(6, 7)));
-
-        arena.setPowerUps(powerUps);
-
-        assertEquals(2, arena.getPowerUps().size());
-        assertTrue(arena.getPowerUps().contains(new PowerUp(new Position(5, 5))));
-        assertTrue(arena.getPowerUps().contains(new PowerUp(new Position(6, 7))));
     }
 
     @Test
@@ -90,22 +58,6 @@ public class ArenaTest {
     }
 
     @Test
-    void testAddCoin() {
-        Arena arena = new Arena(20, 15);
-        Coin coin = new Coin(new Position(10,10));
-        arena.addCoin(coin);
-        assertTrue(arena.getCoins().contains(new Coin(new Position(10,10))));
-    }
-
-    @Test
-    void testAddPowerUp() {
-        Arena arena = new Arena(20, 15);
-        PowerUp powerUp = new PowerUp(new Position(10,10));
-        arena.addPowerUp(powerUp);
-        assertTrue(arena.getPowerUps().contains(new PowerUp(new Position(10,10))));
-    }
-
-    @Test
     void testSetPacmanPosition() {
         Arena arena = new Arena(20, 15);
         Position position = new Position(10,10);
@@ -114,10 +66,67 @@ public class ArenaTest {
     }
 
     @Test
-    void testSetGhostPosition() {
+    void testSetAndGetCollectibles() {
         Arena arena = new Arena(20, 15);
-        Position position = new Position(10,10);
-        arena.setGhostPosition(arena.getBlinky(), position);
-        assertEquals(position, arena.getBlinky().getPosition());
+        HashSet<Collectible> testCollectibles = new HashSet<>();
+
+        Collectible c1 = new Apple(new Position(10,10));
+        Collectible c2 = new Cherry(new Position(5,5));
+        Collectible c3 = new Strawberry(new Position(1,1));
+        Collectible c4 = new Coin(new Position(15,15));
+        Collectible c5 = new Key(new Position(2,9));
+        Collectible c6 = new Orange(new Position(2,3));
+        testCollectibles.add(c1);
+        testCollectibles.add(c2);
+        testCollectibles.add(c3);
+        testCollectibles.add(c4);
+        testCollectibles.add(c5);
+        testCollectibles.add(c6);
+        arena.setCollectibles(testCollectibles);
+
+        assertEquals(testCollectibles, arena.getCollectibles());
+        assertTrue(arena.getCollectibles().contains(c1));
+        assertTrue(arena.getCollectibles().contains(c2));
+        assertTrue(arena.getCollectibles().contains(c3));
+        assertTrue(arena.getCollectibles().contains(c4));
     }
+
+    @Test
+    void testSetAndGetGhosts() {
+        Arena arena = new Arena(20, 15);
+        HashSet<Ghost> testGhost = new HashSet<>();
+
+        Ghost g1 = new Clyde(new Position(10,10));
+        Ghost g2 = new Pinky(new Position(5,5));
+        Ghost g3 = new Inky(new Position(1,1));
+        Ghost g4 = new Blinky(new Position(15,15));
+        testGhost.add(g1);
+        testGhost.add(g2);
+        testGhost.add(g3);
+        testGhost.add(g4);
+        arena.setGhosts(testGhost);
+
+        assertEquals(testGhost, arena.getGhosts());
+        assertTrue(arena.getGhosts().contains(g1));
+        assertTrue(arena.getGhosts().contains(g2));
+        assertTrue(arena.getGhosts().contains(g3));
+        assertTrue(arena.getGhosts().contains(g4));
+    }
+
+    @Test
+    void testAddCollectible() {
+        Arena arena = new Arena(20,20);
+        Collectible collectible = new Apple(new Position(10,10));
+        arena.addCollectible(collectible);
+        assertTrue(arena.getCollectibles().contains(collectible));
+    }
+
+    @Test
+    void testAddGhost() {
+        Arena arena = new Arena(20,20);
+        Ghost ghost = new Blinky(new Position(10,10));
+        arena.addGhost(ghost);
+        assertTrue(arena.getGhosts().contains(ghost));
+    }
+
 }
