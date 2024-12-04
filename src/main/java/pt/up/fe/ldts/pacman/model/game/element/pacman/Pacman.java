be4974d7ae209fcec97b83bf00ecd1b2031887f4
Pedro Castro <falecompedroac@gmail.com>
2024-12-04 20:58:43 +0000

    implemented lifes for Pacman

--- src/main/java/pt/up/fe/ldts/pacman/model/game/element/pacman/Pacman.java
+++ src/main/java/pt/up/fe/ldts/pacman/model/game/element/pacman/Pacman.java
@@ -4,7 +4,14 @@ import pt.up.fe.ldts.pacman.model.game.Position;
 import pt.up.fe.ldts.pacman.model.game.element.MovableElement;
 
 public class Pacman extends MovableElement {
+    private int life;
+
     public Pacman(Position pos) {
         super(pos);
+        this.life = 3;
+    }
+
+    public void decreaseLife() {
+        this.life--;
     }
 }
