4956f0bf7a2f4816dc15a8c03b4d4795e2b92dff
Pedro Castro <falecompedroac@gmail.com>
2024-12-12 17:24:14 +0000

    created menu controllers tests

---
+++ src/test/java/pt/up/fe/ldts/pacman/controller/menu/MainMenuControllerTest.java
@@ -0,0 +1,52 @@
+package pt.up.fe.ldts.pacman.controller.menu;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.up.fe.ldts.pacman.Game;
+import pt.up.fe.ldts.pacman.States.MapSelectionMenuState;
+import pt.up.fe.ldts.pacman.gui.GUI;
+import pt.up.fe.ldts.pacman.model.menu.MainMenu;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.mockito.Mockito.*;
+
+class MainMenuControllerTest {
+    private MainMenu mainMenu;
+    private Game game;
+    private MainMenuController controller;
+
+    @BeforeEach
+    void setUp() {
+        mainMenu = mock(MainMenu.class);
+        game = mock(Game.class);
+        controller = new MainMenuController(mainMenu);
+    }
+
+    @Test
+    void testSelectPreviousOption() throws IOException, URISyntaxException {
+        controller.step(game, GUI.ACTION.UP, 0);
+        verify(mainMenu, times(1)).selectPreviousOption();
+    }
+
+    @Test
+    void testSelectNextOption() throws IOException, URISyntaxException {
+        controller.step(game, GUI.ACTION.DOWN, 0);
+        verify(mainMenu, times(1)).selectNextOption();
+    }
+
+    @Test
+    void testStartSelected() throws Exception {
+        when(mainMenu.StartSelected()).thenReturn(true);
+        controller.step(game, GUI.ACTION.SELECT, 0);
+        verify(game, times(1)).setState(any(MapSelectionMenuState.class));
+    }
+
+    @Test
+    void testExitSelected() throws Exception {
+        when(mainMenu.ExitSelected()).thenReturn(true);
+        controller.step(game, GUI.ACTION.SELECT, 0);
+        verify(game, times(1)).setState(null);
+    }
+}
---
+++ src/test/java/pt/up/fe/ldts/pacman/controller/menu/PauseMenuControllerTest.java
@@ -0,0 +1,54 @@
+package pt.up.fe.ldts.pacman.controller.menu;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.up.fe.ldts.pacman.Game;
+import pt.up.fe.ldts.pacman.States.MainMenuState;
+import pt.up.fe.ldts.pacman.States.State;
+import pt.up.fe.ldts.pacman.gui.GUI;
+import pt.up.fe.ldts.pacman.model.menu.PauseMenu;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.mockito.Mockito.*;
+
+class PauseMenuControllerTest {
+    private PauseMenu pauseMenu;
+    private Game game;
+    private PauseMenuController controller;
+
+    @BeforeEach
+    void setUp() {
+        pauseMenu = mock(PauseMenu.class);
+        game = mock(Game.class);
+        controller = new PauseMenuController(pauseMenu);
+    }
+
+    @Test
+    void testSelectPreviousOption() throws IOException, URISyntaxException {
+        controller.step(game, GUI.ACTION.UP, 0);
+        verify(pauseMenu, times(1)).selectPreviousOption();
+    }
+
+    @Test
+    void testSelectNextOption() throws IOException, URISyntaxException {
+        controller.step(game, GUI.ACTION.DOWN, 0);
+        verify(pauseMenu, times(1)).selectNextOption();
+    }
+
+    @Test
+    void testResumeSelected() throws Exception {
+        when(pauseMenu.ResumeSelected()).thenReturn(true);
+        controller.step(game, GUI.ACTION.SELECT, 0);
+        State<?> ps = verify(pauseMenu, times(1)).getPausedState();
+        verify(game, times(1)).setState(ps);
+    }
+
+    @Test
+    void testExitSelected() throws Exception {
+        when(pauseMenu.ExitSelected()).thenReturn(true);
+        controller.step(game, GUI.ACTION.SELECT, 0);
+        verify(game, times(1)).setState(any(MainMenuState.class));
+    }
+}
