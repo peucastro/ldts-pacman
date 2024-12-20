46547e68816327a494a6f4614812efaf8fa2685b
PauloSaa29 <120606883+PauloSaa29@users.noreply.github.com>
2024-12-20 04:05:35 +0000

    MainMenuController Tests

--- src/test/java/pt/up/fe/ldts/pacman/controller/menu/MainMenuControllerTest.java
+++ src/test/java/pt/up/fe/ldts/pacman/controller/menu/MainMenuControllerTest.java
@@ -4,29 +4,81 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pt.up.fe.ldts.pacman.Game;
 import pt.up.fe.ldts.pacman.audio.AudioManager;
+import pt.up.fe.ldts.pacman.audio.AudioPlayer;
+import pt.up.fe.ldts.pacman.model.game.element.Direction;
+import pt.up.fe.ldts.pacman.model.menu.MapSelectionMenu;
 import pt.up.fe.ldts.pacman.states.menu.MapSelectionMenuState;
 import pt.up.fe.ldts.pacman.gui.GUI;
+import pt.up.fe.ldts.pacman.model.Position;
+import pt.up.fe.ldts.pacman.model.game.element.ghost.Blinky;
+import pt.up.fe.ldts.pacman.model.game.element.ghost.Clyde;
+import pt.up.fe.ldts.pacman.model.game.element.ghost.Inky;
+import pt.up.fe.ldts.pacman.model.game.element.ghost.Pinky;
 import pt.up.fe.ldts.pacman.model.menu.MainMenu;
 
-import java.awt.*;
+import static org.mockito.Mockito.*;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Arrays;
+import java.awt.FontFormatException;
 import java.util.List;
-
-import static org.mockito.Mockito.*;
 
 class MainMenuControllerTest {
     private MainMenu mainMenu;
     private Game game;
     private MainMenuController controller;
+    private Blinky blinky;
+    private Inky inky;
+    private Pinky pinky;
+    private Clyde clyde;
 
     @BeforeEach
     void setUp() {
         mainMenu = mock(MainMenu.class);
         game = mock(Game.class);
-        controller = new MainMenuController(mainMenu, AudioManager.getInstance());
+
+        AudioManager audioManager = mock(AudioManager.class);
+
+        AudioPlayer menuSelect = mock(AudioPlayer.class);
+        AudioPlayer menuConfirmSelection = mock(AudioPlayer.class);
+
+        when(audioManager.getAudio("menuSelect")).thenReturn(menuSelect);
+        when(audioManager.getAudio("menuConfirmSelection")).thenReturn(menuConfirmSelection);
+
+        when(menuSelect.getVolume()).thenReturn(0.25f);
+        when(menuConfirmSelection.getVolume()).thenReturn(0.2f);
+
+        when(game.getAudioManager()).thenReturn(audioManager);
+
+        controller = new MainMenuController(mainMenu, audioManager);
+
+        MapSelectionMenu mapSelectionMenu = mock(MapSelectionMenu.class);
+        MapSelectionMenuController mapSelectionMenuController = new MapSelectionMenuController(mapSelectionMenu, audioManager);
+
+        MapSelectionMenuState mapSelectionMenuState = mock(MapSelectionMenuState.class);
+        when(mapSelectionMenuState.createController(audioManager)).thenReturn(mapSelectionMenuController);
+
+        blinky = mock(Blinky.class);
+        inky = mock(Inky.class);
+        pinky = mock(Pinky.class);
+        clyde = mock(Clyde.class);
+
+        Position blinkyPosition = new Position(3, 4);
+        Position inkyPosition = new Position(5, 8);
+        Position pinkyPosition = new Position(19, 13);
+        Position clydePosition = new Position(24, 3);
+
+        when(blinky.getPosition()).thenReturn(blinkyPosition);
+        when(inky.getPosition()).thenReturn(inkyPosition);
+        when(pinky.getPosition()).thenReturn(pinkyPosition);
+        when(clyde.getPosition()).thenReturn(clydePosition);
+
+        when(mainMenu.getBlinky()).thenReturn(blinky);
+        when(mainMenu.getInky()).thenReturn(inky);
+        when(mainMenu.getPinky()).thenReturn(pinky);
+        when(mainMenu.getClyde()).thenReturn(clyde);
     }
+
 
     @Test
     void testSelectPreviousOption() throws IOException, URISyntaxException, FontFormatException {
@@ -53,4 +105,91 @@ class MainMenuControllerTest {
         controller.step(game, List.of(GUI.ACTION.SELECT), 0);
         verify(game, times(1)).setState(null);
     }
+
+    @Test
+    void testResolutionSelected() throws Exception {
+        when(mainMenu.ResolutionSelected()).thenReturn(true);
+
+        when(game.getResolution()).thenReturn(GUI.SCREEN_RESOLUTION._900p);
+
+        controller.step(game, List.of(GUI.ACTION.SELECT), 0);
+
+        verify(game, times(1)).setResolution(GUI.SCREEN_RESOLUTION._1080p);
+        verify(mainMenu, times(1)).setResolution(GUI.SCREEN_RESOLUTION._1080p);
+    }
+
+    @Test
+    void testMasterVolumeSelected() throws Exception {
+        GUI gui = mock(GUI.class);
+        when(game.getGui()).thenReturn(gui);
+
+        when(mainMenu.MasterVolumeSelected()).thenReturn(true);
+
+        when(game.getAudioManager().getMasterVolume()).thenReturn(0.5f);
+
+        controller.step(game, List.of(GUI.ACTION.SELECT), 0);
+
+        verify(mainMenu, times(1)).setMasterVolume(anyFloat());
+    }
+
+    @Test
+    void testGhostMovementBlinky() throws Exception {
+        when(blinky.getPosition()).thenReturn(new Position(3, 4));
+
+        controller.step(game, List.of(), 0);
+
+        verify(blinky, times(1)).setDirection(Direction.RIGHT);
+
+        when(blinky.getPosition()).thenReturn(new Position(7, 4));
+
+        controller.step(game, List.of(), 0);
+
+        verify(blinky, times(1)).setDirection(Direction.LEFT);
+    }
+
+    @Test
+    void testGhostMovementInky() throws Exception {
+        when(inky.getPosition()).thenReturn(new Position(5, 8));
+
+        controller.step(game, List.of(), 0);
+
+        verify(inky, times(1)).setDirection(Direction.DOWN);
+
+        when(inky.getPosition()).thenReturn(new Position(5, 13));
+
+        controller.step(game, List.of(), 0);
+
+        verify(inky, times(1)).setDirection(Direction.UP);
+    }
+
+    @Test
+    void testGhostMovementPinky() throws Exception {
+        when(pinky.getPosition()).thenReturn(new Position(19, 13));
+
+        controller.step(game, List.of(), 0);
+
+        verify(pinky, times(1)).setDirection(Direction.RIGHT);
+
+        when(pinky.getPosition()).thenReturn(new Position(26, 13));
+
+        controller.step(game, List.of(), 0);
+
+        verify(pinky, times(1)).setDirection(Direction.LEFT);
+    }
+
+    @Test
+    void testGhostMovementClyde() throws Exception {
+        when(clyde.getPosition()).thenReturn(new Position(24, 3));
+
+        controller.step(game, List.of(), 0);
+
+        verify(clyde, times(1)).setDirection(Direction.DOWN);
+
+        when(clyde.getPosition()).thenReturn(new Position(24, 10));
+
+        controller.step(game, List.of(), 0);
+
+        verify(clyde, times(1)).setDirection(Direction.UP);
+    }
+
 }
