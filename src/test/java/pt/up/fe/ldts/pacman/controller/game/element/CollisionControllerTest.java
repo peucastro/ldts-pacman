3930f1af6a5f26aefd018376820f8d31e5a6d306
Gustavo Madureira <up202304978@up.pt>
2024-12-21 21:47:19 +0000

    CollisionController tests

--- src/test/java/pt/up/fe/ldts/pacman/controller/game/element/CollisionControllerTest.java
+++ src/test/java/pt/up/fe/ldts/pacman/controller/game/element/CollisionControllerTest.java
@@ -3,6 +3,7 @@ package pt.up.fe.ldts.pacman.controller.game.element;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pt.up.fe.ldts.pacman.Game;
+import pt.up.fe.ldts.pacman.MockAudio;
 import pt.up.fe.ldts.pacman.audio.AudioManager;
 import pt.up.fe.ldts.pacman.audio.AudioPlayer;
 import pt.up.fe.ldts.pacman.model.Position;
@@ -11,7 +12,9 @@ import pt.up.fe.ldts.pacman.model.game.element.collectibles.Collectible;
 import pt.up.fe.ldts.pacman.model.game.element.collectibles.PowerUp;
 import pt.up.fe.ldts.pacman.model.game.element.ghost.Ghost;
 import pt.up.fe.ldts.pacman.model.game.element.ghost.GhostState;
+import pt.up.fe.ldts.pacman.model.game.element.ghost.Pinky;
 import pt.up.fe.ldts.pacman.model.game.element.pacman.Pacman;
+import pt.up.fe.ldts.pacman.states.game.DyingState;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -19,6 +22,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class CollisionControllerTest {
@@ -109,5 +113,38 @@ public class CollisionControllerTest {
         verify(ghost).setState(GhostState.ALIVE);
         verify(ghost).setSpeed(Arena.GHOST_NORMAL_SPEED);
         verify(pacman).setSpeed(Arena.PACMAN_NORMAL_SPEED);
+    }
+
+    @Test
+    void testDyingState() throws IOException, URISyntaxException {
+        when(arena.getPacmans()).thenReturn(List.of(new Pacman(new Position(0,0))));
+        when(arena.getGhosts()).thenReturn(Set.of(new Pinky(new Position(0,0))));
+        AudioManager audioManager = MockAudio.getMockAudioManager();
+        when(game.getAudioManager()).thenReturn(audioManager);
+
+        collisionController.step(game, List.of(), 0);
+
+        verify(game, times(1)).setState(any(DyingState.class));
+    }
+
+    @Test
+    void multiplayerPacmanRespawns() throws IOException, URISyntaxException {
+        Pacman pacman1 = new Pacman(new Position(0,0));
+        pacman1.setRespawnPosition(new Position(100,100));
+        Pacman pacman2 = new Pacman(new Position(100,100));
+        when(arena.getPacmans()).thenReturn(List.of(pacman1,pacman2));
+        when(arena.getGhosts()).thenReturn(Set.of(new Pinky(new Position(0,0))));
+        AudioManager audioManager = MockAudio.getMockAudioManager();
+        when(game.getAudioManager()).thenReturn(audioManager);
+
+        //simulate the wait while the pacman is dead
+        for(int i = 0; i < 110; ++i) collisionController.step(game, List.of(), 0);
+
+        assertTrue(pacman1.isDying());
+
+        //frame when pacman1 respawns
+        collisionController.step(game, List.of(), 0);
+
+        assertFalse(pacman1.isDying());
     }
 }
